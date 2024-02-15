fb67c4cd19f02bc542f223cf63a3b152a7ae6e68
Demian <damianlopezou@gmail.com>
2024-02-15 20:17:50 +0100

    fix: schema

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,26 +8,47 @@ import { CreateBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
-  const { userId } = auth();
+  const { userId, orgId } = auth();
 
-  if (!userId) {
+  if (!userId || !orgId) {
     return {
       error: "Unauthorized",
     };
   }
 
-  const { title } = data;
+  const { title, image } = data;
+  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHtml, imageUserName] =
+    image.split("|");
+
+  if (
+    !imageId ||
+    !imageThumbUrl ||
+    !imageFullUrl ||
+    !imageLinkHtml ||
+    !imageUserName
+  ) {
+    return {
+      error: "Missing fields. Failed to create board",
+    };
+  }
+
   let board;
 
   try {
     board = await db.board.create({
       data: {
         title,
+        orgId,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageLinkHtml,
+        imageUserName,
       },
     });
   } catch (error) {
     return {
-      error: "Database error",
+      error: "Failed to create",
     };
   }
 
