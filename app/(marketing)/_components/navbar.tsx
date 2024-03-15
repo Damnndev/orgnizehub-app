19bdec498ef8948b3e10806b3381ada9839069a7
Demian <damianlopezou@gmail.com>
2024-03-15 21:16:18 +0100

    feat: update home page view

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const NavBar = () => {
   return (
-    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+    <div className="hidden md:flex fixed top-0 w-full h-14 px-4  bg-white items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
@@ -12,7 +12,7 @@ export const NavBar = () => {
             <Link href="/sign-in">Log in</Link>
           </Button>
           <Button size="sm" asChild>
-            <Link href="/sign-up">OrganizeHub for free</Link>
+            <Link href="/sign-up">Sign up</Link>
           </Button>
         </div>
       </div>
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { Medal } from "lucide-react";
 import { Poppins } from "next/font/google";
 import localFont from "next/font/local";
+import Image from "next/image";
 import Link from "next/link";
-
+import illustration from "../../public/home-img.webp";
 const headingFont = localFont({ src: "../../public/fonts/font.woff2" });
 const textFont = Poppins({
   subsets: ["latin"],
@@ -13,27 +13,42 @@ const textFont = Poppins({
 
 const MarketingPage = () => {
   return (
-    <div className="flex items-center justify-center flex-col">
-      <div className={cn("flex items-center justify-center flex-col", headingFont.className)}>
-        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
-          <Medal className="h-6 w-6 mr-2" />
-          No 1 task management
-        </div>
-        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+    <div className="min-h-screen flex flex-col md:grid md:grid-cols-2 items-center justify-center px-4 py-8 mx-auto max-w-7xl gap-4 md:gap-8">
+      <Image
+        alt="Home illustration"
+        src={illustration}
+        layout="responsive"
+        width={4268}
+        height={3200}
+        objectFit="cover"
+      />
+      <div
+        className={cn(
+          "flex flex-col items-center space-y-4",
+          headingFont.className
+        )}
+      >
+        <h1 className="text-2xl sm:text-3xl text-center text-neutral-800">
           OrganizeHub helps to organize your tasks
         </h1>
-        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-          work organized
+        <div
+          className={`text-sm text-center text-neutral-400 ${textFont.className}`}
+        >
+          Collaborate, manage projects, and reach new productivity peaks.
+          Accomplish it all with OrganizeHub.
+        </div>
+        <div className="flex items-center justify-center space-x-4 md:block md:w-auto w-full">
+          <Button className="md:hidden" size="sm" variant="outline" asChild>
+            <Link href="/sign-in">Log in</Link>
+          </Button>
+          <Button className="md:hidden" size="sm" asChild>
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+          <Button className="hidden md:flex mt-4" size="lg" asChild>
+            <Link href="/sign-up"> Get Started</Link>
+          </Button>
         </div>
       </div>
-      <div className={cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto", textFont.className)}>
-        Collaborate, manage projects, and reach new productivity peaks. From
-        high rises to the home office, the way your team works is
-        unique—accomplish it all with OrganizeHub.
-      </div>
-      <Button className="mt-6" size="lg" asChild>
-        <Link href="/sign-up"> Get a trial</Link>
-      </Button>
     </div>
   );
 };
